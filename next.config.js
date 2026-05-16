08589970eb4d85741c9df7f417e2dfe230bcc5c1
danielsilvaogp-oss <danielsilvaogp@gmail.com>
2026-05-16 08:17:48 -0400

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -4,12 +4,7 @@ const nextConfig = {
   images: {
     domains: ['localhost'],
   },
-  // Ignoramos errores de tipos para que el despliegue de la Hammer Academy sea rápido
-  typescript: {
-    ignoreBuildErrors: true,
-  },
-  webpack: (config, { isServer, webpack }) => {
-    // Solución para módulos de Node en el cliente
+  webpack: (config, { isServer }) => {
     if (!isServer) {
       config.resolve.fallback = {
         ...config.resolve.fallback,
@@ -17,28 +12,7 @@ const nextConfig = {
         path: false,
         crypto: false,
       };
-
-      // REEMPLAZO DINÁMICO: Evita el error "Cannot find module './tflite_web_api_client'" en el navegador
-      config.plugins.push(
-        new webpack.NormalModuleReplacementPlugin(
-          /tflite_web_api_client/,
-          (resource) => {
-            resource.request = resource.request.replace(
-              /.*tflite_web_api_client.*/,
-              'path' // Reemplazamos por un módulo inofensivo
-            );
-          }
-        )
-      );
     }
-
-    // ALIAS: Engañamos a Webpack para que no busque los archivos inexistentes de TFLite
-    config.resolve.alias = {
-      ...config.resolve.alias,
-      './tflite_web_api_client': false,
-      '../tflite_web_api_client': false,
-    };
-
     return config;
   },
 }
